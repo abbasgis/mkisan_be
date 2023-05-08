eea1a22c1a8afce930f87139611f6c9bc3a3f756
abbasgis@gmail.com <admin123@abc>
2023-05-08 15:27:17 +0500

    setting.py

--- mkisan_be/settings.py
+++ mkisan_be/settings.py
@@ -14,16 +14,16 @@ from datetime import timedelta
 from pathlib import Path
 
 from api.local_settings import API_APPS
-# if os.name == 'nt':
-#     import platform
-#     OSGEO4W = r"C:\OSGeo4W"
-#     if '64' in platform.architecture()[0]:
-#         OSGEO4W += "64"
-#     assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
-#     os.environ['OSGEO4W_ROOT'] = OSGEO4W
-#     os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
-#     os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
-#     os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
+if os.name == 'nt':
+    import platform
+    OSGEO4W = r"C:\OSGeo4W"
+    if '64' in platform.architecture()[0]:
+        OSGEO4W += "64"
+    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
 
 
 
@@ -107,11 +107,11 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'mera_kisan',
         'USER': 'postgres',  # Not used with sqlite3.
-        # 'PASSWORD': '123',  # Not used with sqlite3.
-        # 'HOST': 'localhost',  # Set to empty string for localhost. Not used with sqlite3.
-        'PASSWORD': 'pg123@meraKisan',  # Not used with sqlite3.
-        'HOST': '65.1.240.23',  # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432'
+        'PASSWORD': '123',  # Not used with sqlite3.
+        'HOST': 'localhost',  # Set to empty string for localhost. Not used with sqlite3.
+        # 'PASSWORD': 'pg123@meraKisan',  # Not used with sqlite3.
+        # 'HOST': '65.1.240.23',  # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5433'
     }
 }
 
