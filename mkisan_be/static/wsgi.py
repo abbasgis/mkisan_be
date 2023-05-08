9d2b62e6ff74324c80be16adc53cccbe884ca70e
abbasgis@gmail.com <admin123@abc>
2023-05-08 21:12:48 +0500

    setting.py

---
+++ mkisan_be/static/wsgi.py
@@ -0,0 +1,29 @@
+"""
+WSGI config for mkisan_be project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+import os
+import sys
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+from django.core.wsgi import get_wsgi_application
+from mkisan_be import settings
+
+sys.path.append('/home/python-www/mkisan_be')
+sys.path.append('/home/python-www/mkisan_be/venv/lib/python3.8/site-packages')
+
+#
+# # add the virtualenv site-packages path to the sys.path
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mkisan_be.settings")
+
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mkisan_be.settings")
+#
+# application = get_wsgi_application()
